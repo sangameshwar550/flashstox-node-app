276533e856b0ad232161f86cc5fc4dc480a5ea12
dev sanga <sangameshwar550@gmail.com>
2026-04-25 16:11:50 +0530

    hellow world app

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 8081;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World from Google Cloud Run!\n");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
